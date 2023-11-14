f426cd89bde170200197dcf8b50a3f4c995ecaf1
gimugibomu <wolfbone0304@naver.com>
2023-11-14 14:07:58 +0900

    gibomi

--- app/src/main/java/com/example/study_friend/SignUp.java
+++ app/src/main/java/com/example/study_friend/SignUp.java
@@ -1,16 +1,25 @@
 package com.example.study_friend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.study_friend.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "signUP";
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +36,18 @@ public class SignUp extends AppCompatActivity {
             });
             builder.show(); // 다이얼로그 표시
         });
+    }
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG,"CreateUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        }
+                    }
+                });
     }
 }
