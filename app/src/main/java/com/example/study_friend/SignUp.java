c09453ebe24e70601368c892f66c9056c5c17d92
gimugibomu <wolfbone0304@naver.com>
2023-11-21 21:26:14 +0900

    gibomi

--- app/src/main/java/com/example/study_friend/SignUp.java
+++ app/src/main/java/com/example/study_friend/SignUp.java
@@ -17,6 +17,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -47,27 +48,34 @@ public class SignUp extends AppCompatActivity {
             String nickname = binding.editNickname.getText().toString().trim();
             Log.d(TAG,nickname);
             if(nickname != null){
-                CollectionReference userRef = db.collection("FirebaseID.user");
-                Query query = userRef.whereEqualTo("FirebaseID.nickname",nickname);
-                Log.d(TAG,"여기까지는 성공");
-                Task<QuerySnapshot>task = query.get();
-                QueryDocumentSnapshot document;
-//                CollectionReference cf = db.collection("FirebaseID.user");
-//                cf.whereEqualTo("FirebaseID.nickname",nickname)
-//                        .get()
-//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                                if(task.isSuccessful()){
-//                                    Log.d(TAG,"불러는 들어왔다");//진짜 불러는 들어왔다.
-//                                    for(QueryDocumentSnapshot document : task.getResult()){
-//                                        Log.d(TAG,document.getId() + "=>" + document.getData());
-//                                    }
-//                                }else{
-//                                    Log.d(TAG,"Error",task.getException());
-//                                }
-//                            }
-//                        });
+//                CollectionReference userRef = db.collection("FirebaseID.user");
+//                Query query = userRef.whereEqualTo("FirebaseID.nickname",nickname);
+//                Log.d(TAG,"여기까지는 성공");
+//                Task<QuerySnapshot>task = query.get();
+//                QueryDocumentSnapshot document;
+                db.collection("users")
+                        .whereEqualTo("nickname","gibomi")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    Log.d(TAG,"불러는 들어왔다");//진짜 불러는 들어왔다.
+                                    QuerySnapshot snapshots = task.getResult();
+                                    Log.d("snapshots",snapshots.toString());
+                                    List<DocumentChange> documentChanges = snapshots.getDocumentChanges();
+                                    Log.d("snapshots",documentChanges.toString());
+                                    for(DocumentChange documentChange : documentChanges){
+                                        Log.d(TAG,"인간적으로 이건 해라");
+                                    }
+                                    for(QueryDocumentSnapshot document : task.getResult()){
+                                        Log.d(TAG,document.getId() + "=>" + document.getData());
+                                    }
+                                }else{
+                                    Log.d(TAG,"Error",task.getException());
+                                }
+                            }
+                        });
             }
         });
         binding.submitBtn.setOnClickListener(v -> {
@@ -90,13 +98,13 @@ public class SignUp extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
 
                             Map<String,Object> userMap = new HashMap<>();
-                            userMap.put("FirebaseID.documentID",user.getUid());
-                            userMap.put("FirebaseID.email",email);
-                            userMap.put("FirebaseID.nickname",nickname);
-                            userMap.put("FirebaseID.univ",univ);
-                            userMap.put("FirebaseId.major",major);
-                            userMap.put("FirebaseID.semester",semester);
-                            db.collection("FirebaseID.user").document(user.getUid()).set(userMap, SetOptions.merge());
+                            userMap.put("documentID",user.getUid());
+                            userMap.put("email",email);
+                            userMap.put("nickname",nickname);
+                            userMap.put("univ",univ);
+                            userMap.put("major",major);
+                            userMap.put("semester",semester);
+                            db.collection("user").document(user.getUid()).set(userMap, SetOptions.merge());
 
                             AlertDialog.Builder builder = new AlertDialog.Builder(getLayoutInflater().getContext());
                             builder.setTitle("회원가입 성공");
