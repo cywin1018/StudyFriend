2549896ec6a7eae35f22fb5eaecabbe679ae8a8b
cywin1018 <cywin0821@gmail.com>
2023-11-23 20:58:27 +0900

    Dialog created

--- app/src/main/java/com/example/study_friend/StudyContent.java
+++ app/src/main/java/com/example/study_friend/StudyContent.java
@@ -22,8 +22,20 @@ public class StudyContent extends AppCompatActivity {
         binding = ActivityStudyContentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.studyRegBtn.setOnClickListener(v -> {
+            Log.d("yongwon","스터디 신청 버튼 클릭");
+            AlertDialog.Builder menu = new AlertDialog.Builder(StudyContent.this);
 
-
+            String people = "컴퓨터학부";
+            String grade = "1학년";
+            String field = "프로그래밍";
+            String place = "정보과학관 3층 중간 스터디룸";
+            String num = "5명";
+            menu.setTitle("작성 목록");
+            menu.setMessage("모집대상 " + people + "\n" + "학년 " + grade + "\n" + "분야 " + field + "\n" + "장소 " + place + "\n" + "인원 " + num + "\n");
+            menu.setPositiveButton("Ok",null);
+            menu.setNegativeButton("Cancel",null);
+            menu.create();
+            menu.show();
         });
     }
 
